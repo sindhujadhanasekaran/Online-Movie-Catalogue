b6677a0f731f868ef47e0ccaa382eb7c39e82d0b
s.o.dhanasekaran <s.o.dhanasekaran@M2C-L-3V2K2B3>
2023-01-19 12:05:46 +0530

    Add project files.

---
+++ Online Movie Catalogue/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Online_Movie_Catalogue.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
